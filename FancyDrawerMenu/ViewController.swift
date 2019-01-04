f42be3e3a53d71b5249b134781f423c87e478e03
Sonar <Sonar@imacs-imac.local>
2019-01-04 17:37:14 -0500

    Initial Commit

---
+++ FancyDrawerMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FancyDrawerMenu
+//
+//  Created by Sonar on 1/4/19.
+//  Copyright © 2019 Sonar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
