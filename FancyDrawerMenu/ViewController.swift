315770178cc2ada941969063faf9fa4cdd4a9820
Sonar <Sonar@imacs-imac.local>
2019-01-05 17:59:04 -0500

    quick little update

--- FancyDrawerMenu/ViewController.swift
+++ FancyDrawerMenu/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewTop: NSLayoutConstraint!
+    @IBOutlet weak var viewLeading: NSLayoutConstraint!
+    @IBOutlet weak var viewTrailing: NSLayoutConstraint!
+    @IBOutlet weak var viewBottom: NSLayoutConstraint!
+    @IBOutlet weak var mainView: UIView!
+    
+    @IBOutlet weak var userImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        userImg.layer.cornerRadius = userImg.frame.size.height/2
     }
 
-
+    @IBAction func openMenu(_ sender: UIButton) {
+        if (!sender.isSelected) {
+            UIView.animate(withDuration: 0.5) {
+                self.viewTop.constant = 50
+                self.viewLeading.constant = 280
+                self.viewBottom.constant = -50
+                self.viewTrailing.constant = 280
+                self.mainView.layer.cornerRadius = 7
+                self.view.layoutIfNeeded()
+            }
+        }
+        else {
+            UIView.animate(withDuration: 0.5) {
+                self.viewTop.constant = 0
+                self.viewLeading.constant = 0
+                self.viewBottom.constant = 0
+                self.viewTrailing.constant = 0
+                self.mainView.layer.cornerRadius = 0
+                self.view.layoutIfNeeded()
+            }
+        }
+        sender.isSelected = !sender.isSelected
+    }
+    
+    
 }
 
